9811f57ca77e322f087f45ba69205877fd4a702c
SaraBroad <saraebroad@gmail.com>
2024-08-01 21:06:13 -0400

    add sample data

---
+++ src/data.ts
@@ -0,0 +1,37 @@
+const destinations = [
+    {
+      cityName: "Barcelona",
+      dateArriving: "6/25/2025",
+      arrivalMethod: "plane",
+      activities: [
+        {
+          name: "Park Guell",
+          timeNeededInHours: 2,
+          cost: "25 euros",
+        },
+        {
+          name: "Sagrada Familia",
+          timeNeededInHours: 1,
+          cost: "15 euros",
+        },
+      ]
+    },
+    {
+      cityName: "Seville",
+      dateArriving: "6/29/2025",
+      arrivalMethod: "plane",
+      activities: [
+        {
+          name: "Seville Cathedral",
+          timeNeededInHours: 1,
+          cost: "12 euros",
+        },
+        {
+          name: "Alcazar",
+          timeNeededInHours: 2,
+          cost: "15 euros",
+        },
+      ]
+    },
+  ];
+  
